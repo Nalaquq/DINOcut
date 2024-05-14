77f66f7d29d1696ab00f59931cbac4319cc04614
nalkuq <seangleason52@gmail.com>
2024-05-14 18:34:33 -0400

    removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import VisualFlow as vf
-
-vf.to_coco(in_format='yolo',
-       images='path/to/images',
-       annotations='path/to/annotations',
-       class_file='path/to/classes.txt',
-       output_file_path='path/to/output.json')
