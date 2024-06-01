847a5b24861bbf2e7ccfa512bc6ad72e60aca577
nalkuq <seangleason52@gmail.com>
2024-06-01 15:38:53 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup  # Changed from distutils.core to setuptools
 import os
 import urllib.request
 
+
+repository_url = "https://github.com/IDEA-Research/GroundingDINO.git"
+commit_hash = "57535c5a79791cb76e36fdb64975271354f10251"
+package_name = "GroundingDINO"
+
 def download_weight_file(url, dest_path):
     if not os.path.exists(dest_path):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -12,6 +17,7 @@ def download_weight_file(url, dest_path):
     else:
         print(f"File {dest_path} already exists.")
 
+'''
 setup(name='DinoCut',
       version='1.0',
       description='Image Processing Pipeline for Synthetic Data Generation',
@@ -20,6 +26,7 @@ setup(name='DinoCut',
       url='https://github.com/Nalaquq/DINOcut',
       packages=['DINOcut'],  # Replace 'my_package' with the actual package name
      )
+'''
 
 def run_command(command: str) -> None:
     try:
@@ -43,22 +50,21 @@ def install_requirements(repo_name: str) -> None:
     requirements_path = repo_name
     run_command(f"{sys.executable} -m pip install -r {requirements_path}")
 
-repository_url = "https://github.com/IDEA-Research/GroundingDINO.git"
-commit_hash = "57535c5a79791cb76e36fdb64975271354f10251"
-package_name = "GroundingDINO"
-
 def main():
     weight_url = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
     dest_path = os.path.join('sam', 'weights', 'sam_vit_h_4b8939.pth')
     download_weight_file(weight_url, dest_path)
 
 if __name__ == "__main__":
-    install_requirements("requirements.txt")
     if not check_package_installed(package_name):
         install_github_repo(repository_url, commit_hash)
     else:
         print(f"{package_name} is already installed.")
-    main()
+    try: 
+        main()
+    except:
+        "SAM weight files are already installed" 
+    install_requirements("requirements.txt")
 
 
 
